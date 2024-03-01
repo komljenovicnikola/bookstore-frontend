d9a7e08e38cc8de3284bf0aa7ccedd6b1287d2e8
Nikola Komljenovic <nikola.komljenovic@flow.space>
2024-03-01 15:06:31 +0100

    add test cases for LoginForm and RegistrationForm

---
+++ src/components/LoginForm/LoginForm.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter as Router } from 'react-router-dom';
+import LoginForm from './LoginForm';
+
+
+jest.mock('../../services/apiService', () => ({
+  login: jest.fn(),
+}));
+
+describe('LoginForm component', () => {
+  it('updates email and password fields when typing', () => {
+    const { getByLabelText } = render(
+      <Router>
+        <LoginForm />
+      </Router>
+    );
+    const emailInput = getByLabelText('Email');
+    const passwordInput = getByLabelText('Password');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+    expect(emailInput.value).toBe('test@example.com');
+    expect(passwordInput.value).toBe('password123');
+  });
+});
---
+++ src/components/RegistrationForm/RegistrationForm.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RegistrationForm from './RegisterationForm';
+
+jest.mock('../../services/apiService', () => ({
+    register: jest.fn(),
+}));
+
+describe('RegistrationForm component', () => {
+    it('updates name, email, and password fields when typing', () => {
+        const { getByLabelText } = render(
+            <Router>
+                <RegistrationForm />
+            </Router>
+        );
+        const emailInput = getByLabelText('Email');
+        const firstNameInput = getByLabelText('First Name');
+        const lasttNameInput = getByLabelText('Last Name');
+        const passwordInput = getByLabelText('Password');
+
+        fireEvent.change(firstNameInput, { target: { value: 'John' } });
+        fireEvent.change(lasttNameInput, { target: { value: 'Doe' }});
+        fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+        expect(firstNameInput.value).toBe('John');
+        expect(lasttNameInput.value).toBe('Doe');
+        expect(emailInput.value).toBe('test@example.com');
+        expect(passwordInput.value).toBe('password123');
+    });
+});
